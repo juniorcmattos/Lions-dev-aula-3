de1bca3b14abf2fa10725506a2a7b77e7576e65e
Filipe Paixão <fpedrapaixao@gmail.com>
2025-07-08 21:05:16 -0300

    feat: add initial CRUD operations example in crud.js

---
+++ crud.js
@@ -0,0 +1,32 @@
+// Oq é um crud 
+// C = Create
+// R = Read
+// U = Update
+// D = Delete
+
+// Criar um Elemento no inicio do array
+
+const linguagens = ["Python", "JavaScript", "Java", "C++", "Ruby"];
+
+// Create
+linguagens.push("Go"); // Adiciona "Go" ao final do array
+
+// Read
+console.log(linguagens); // Exibe o array completo
+
+// Update
+linguagens[1] = "TypeScript"; // Atualiza "JavaScript" para "TypeScript"
+
+console.log(`Tchau JS agora é : ${linguagens}`);
+
+// Delete
+const deleted = linguagens.splice(2,3); // Remove tres elementos a partir do índice 2
+
+console.log(deleted)
+
+console.log(linguagens)
+
+
+
+
+
